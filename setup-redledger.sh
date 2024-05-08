c433818aa07278d26f1b1606805721b7b48d1153
Cedric Bell <cedric.bell@nist.gov>
2024-05-08 12:18:29 -0400

    New branch for seting up redledger on a Raspberry Pi, cleaning unused files, updated scripts and Readme accordinly

--- setup-redledger.sh
+++ setup-redledger.sh
@@ -1,26 +1,43 @@
-#!/bin/bash
-echo "### Cloning the Redledger Fabric repository ... ###"
-git clone -b redledger-main https://github.com/usnistgov/redledger-fabric.git
-echo "### Changing directory ... ###"
-cd redledger-fabric
-echo "### cleaning the environment ###"
-make clean-all
-echo "### Pulling the docker images ###" 
-make docker
-echo "### Creating the Redledger fabric binaries ###"
-make native 
-echo "### TODO :Checking if the binaries are created ###"
-cd ..
-echo "### Installing the fabric samples repository ###"
-curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
-echo "### launching install fabric to get binaries and docker images v 2.4.8 to interact with the test-nertwork ###"
-./install-fabric.sh -f 2.5.6 binary docker
-echo "### Replacing binaries from redledger-fabric-samples with those from redledger-fabric ###"
-find bin/ -type f -not -name 'fabric*' -exec rm {} \;
-cp -r redledger-fabric/build/bin/ bin/
+# Clone the redledger-fabric repository
+echo "### Getting the binaries ... ###"
+git clone --single-branch --branch binaries https://github.com/usnistgov/redledger-fabric.git temp_redledger_repo
+
+# Check if the repository was cloned successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to clone the repository."
+    exit 1
+fi
+# Copy the entire bin directory from temp_redledger_repo to redledger-fabric-samples
+cp -r temp_redledger_repo/bin bin/
+# Maybe need to mkdir bin
+
+# Clean up: Remove the temporary repository directory
+rm -rf /path/to/temp_redledger_repo
+
+echo "### Binaries successfully pulled ###"
+
+
+# Pull the Docker images
+# Need to find a way to specify that it needs to pull the ARM images
+echo "### Getting the docker images ... ###"
+docker pull csd773/redledger-fabric-peer
+docker pull csd773/redledger-fabric-orderer
+docker pull csd773/redledger-fabric-tools
+docker pull csd773/redledger-fabric-ca
+docker pull csd773/redledger-fabric-ccenv
+docker pull csd773/redledger-fabric-baseos
+
+echo "### Docker images successfully pulled ###"
+# Need to update the network.sh script with the environment variable pointing at redledger ones instead of hyperledger
+# Need to update the network.sh script to include the ledgertype. Refer to this commit : https://github.com/usnistgov/redledger-fabric-samples/commit/8ff57cef2cc973a70c63ffe56b2b6ccf963e072f
+
+Launch the test network
 echo "### Preparing to launch network ###"
 cd test-network
-echo "### launching network with blockmatrix ledgertype ###"
+echo "### launching network with blockmatrix ledgertype .. ###"
 ./network.sh up createChannel -c mychannel -ca -l blockmatrix
 echo "### Shutting down the network ###"
 ./network.sh down
+
+
+# Need some testing 
