d7d530334b293b3e066d81b59aa8a55e40a48cd8
Cedric Bell <cedric.bell@nist.gov>
2024-09-30 11:08:09 -0400

    refactoring docker-compose files to match new docker images name

--- test-network/network.sh
+++ test-network/network.sh
@@ -62,7 +62,7 @@ function checkPrereqs() {
   # use the fabric tools container to see if the samples and binaries match your
   # docker images
   LOCAL_VERSION=$(peer version | sed -ne 's/^ Version: //p')
-  DOCKER_IMAGE_VERSION=$(docker run --rm csd773/redledger-fabric-tools:$IMAGETAG peer version | sed -ne 's/^ Version: //p')
+  DOCKER_IMAGE_VERSION=$(docker run --rm csd773/redledger-fabric-tools-arm:$IMAGETAG peer version | sed -ne 's/^ Version: //p')
 
   infoln "LOCAL_VERSION=$LOCAL_VERSION"
   infoln "DOCKER_IMAGE_VERSION=$DOCKER_IMAGE_VERSION"
