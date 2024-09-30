c19b3225e19acee32b7515cae775e7eb66cebe7b
Cedric Bell <cedric.bell@nist.gov>
2024-09-30 10:39:44 -0400

    forgot the script

--- setup-redledger.sh
+++ setup-redledger.sh
@@ -1,26 +1,67 @@
 #!/bin/bash
-echo "### Cloning the Redledger Fabric repository ... ###"
-git clone -b redledger-main https://github.com/usnistgov/redledger-fabric.git
-echo "### Changing directory ... ###"
-cd redledger-fabric
-echo "### cleaning the environment ###"
-make clean-all
-echo "### Pulling the docker images ###" 
-make docker
-echo "### Creating the Redledger fabric binaries ###"
-make native 
-echo "### TODO :Checking if the binaries are created ###"
-cd ..
-echo "### Installing the fabric samples repository ###"
-curl -sSLO https://raw.githubusercontent.com/hyperledger/fabric/main/scripts/install-fabric.sh && chmod +x install-fabric.sh
-echo "### launching install fabric to get binaries and docker images v 2.4.8 to interact with the test-nertwork ###"
-./install-fabric.sh -f 2.5.6 binary docker
-echo "### Replacing binaries from redledger-fabric-samples with those from redledger-fabric ###"
-find bin/ -type f -not -name 'fabric*' -exec rm {} \;
-cp -r redledger-fabric/build/bin/ bin/
+
+# Check if architecture is provided as an argument
+if [ -z "$1" ]; then
+    echo "Usage: $0 <architecture>"
+    echo "Please specify architecture: amd64 or arm64"
+    exit 1
+fi
+
+ARCH=$1
+
+# Validate architecture argument
+if [[ "$ARCH" != "amd64" && "$ARCH" != "arm64" ]]; then
+    echo "Invalid architecture: $ARCH"
+    echo "Please specify architecture: amd64 or arm64"
+    exit 1
+fi
+
+echo "### Getting the binaries for $ARCH ... ###"
+git clone --single-branch --branch binaries https://github.com/usnistgov/redledger-fabric.git temp_redledger_repo
+
+# Check if the repository was cloned successfully
+if [ $? -ne 0 ]; then
+    echo "Failed to clone the repository."
+    exit 1
+fi
+
+# Check if bin directory exists, create if not
+if [ ! -d "bin" ]; then
+    mkdir bin
+fi
+
+# Copy the binaries based on the architecture
+cp -r temp_redledger_repo/bin/$ARCH/* bin/
+
+# Clean up: Remove the temporary repository directory
+rm -rf temp_redledger_repo
+
+echo "### Binaries for $ARCH successfully pulled ###"
+
+# Pull the Docker images
+echo "### Getting the docker images for $ARCH ... ###"
+
+if [ "$ARCH" == "amd64" ]; then
+    docker pull csd773/redledger-fabric-peer
+    docker pull csd773/redledger-fabric-orderer
+    docker pull csd773/redledger-fabric-tools
+    docker pull csd773/redledger-fabric-ca
+    docker pull csd773/redledger-fabric-ccenv
+    docker pull csd773/redledger-fabric-baseos
+else
+    docker pull csd773/redledger-fabric-peer-arm
+    docker pull csd773/redledger-fabric-orderer-arm
+    docker pull csd773/redledger-fabric-tools-arm
+    docker pull csd773/redledger-fabric-ca-arm
+    docker pull csd773/redledger-fabric-ccenv-arm
+    docker pull csd773/redledger-fabric-baseos-arm
+fi
+
+echo "### Docker images for $ARCH successfully pulled ###"
+
+# Update the network.sh script (optional)
 echo "### Preparing to launch network ###"
 cd test-network
-echo "### launching network with blockmatrix ledgertype ###"
+echo "### Launching network with blockmatrix ledgertype ... ###"
 ./network.sh up createChannel -c mychannel -ca -l blockmatrix
-echo "### Shutting down the network ###"
-./network.sh down
+echo "### Network launched successfully ###"
